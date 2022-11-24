b6fe2cc27b2e7f0caf0f3cfe4036920f1cebf872
Lucas Marques da Silva <111651260+LucasMKS@users.noreply.github.com>
2022-11-24 00:07:20 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-# Meu primeiro projeto Python!!!  
+# Exemplo de laço
 
-'''
-Comentario em bloco
-'''
-
-# print() = comando de saida
-print("Hello World!")
-
-# Quando quiser guardar uma String!
-nome = "Lucas Marques"
-# Quando quiser guardar um numero inteiro
-idade = 21
-
-# Exibir o meu nome
-print(nome)
-
-# Quando quiser exibir a frase "Minha idade é " completando com o conteúdo da variavel idaide
-# Para contatenar com '+' as 2 partes tem que estar iguais, usa-se str() para transformar idade em string
-print("Minha idade é "+ str(idade))
-print(f"Minha idade é {idade}")
-print("Minha idade é {}".format(idade))
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i não é mais menor do que 6")
